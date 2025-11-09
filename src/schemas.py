3ff948b4b832b90e9b5e257cfde9dffb3593b96e
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2025-11-09 21:36:13 +0200

    updated schemas

--- src/schemas.py
+++ src/schemas.py
@@ -1,1 +1,13 @@
+from pydantic import BaseModel,field_validator
 
+
+class Prompt(BaseModel):
+    message:str
+
+    @field_validator("message")
+    @classmethod
+    def validate_message(value,cls):
+        if type(value)!=str:
+            raise TypeError("Invalid data type ,expected string")
+        
+        return value
