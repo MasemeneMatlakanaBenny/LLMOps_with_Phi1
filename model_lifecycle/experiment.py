df7c322f92abd50e9916d3df53f5215ddf357fea
MasemeneMatlakanaBenny <bennymasemene46@gmail.com>
2025-11-10 08:22:19 +0200

    added the experiment content

--- model_lifecycle/experiment.py
+++ model_lifecycle/experiment.py
@@ -1,1 +1,18 @@
+import mlflow
+from src.configurations_mlflow import load_host,load_exp_name,load_tags,mlflow_client
+
+# create the variables using the imported functions:
+host=load_host()
+exp_name=load_exp_name()
+tags=load_tags()
+
+# get the client:
+client=mlflow_client()
+
+# create the phi1 experiment:
+phi1_exp=client.create_experiment(
+    name=exp_name,
+    tags=tags
+)
+
 
